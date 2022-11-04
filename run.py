4ae997d5ae43f132624845c7be3a3a601fde9b03
DYNOHACKER <114745029+DYNOHACKER@users.noreply.github.com>
2022-11-04 23:45:12 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("main").menu()
+       __import__("faxi3_MAHADI_ENC").menu()
    except Exception as e:
        exit(str(e))
