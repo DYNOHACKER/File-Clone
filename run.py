0f148442f65c8754737f0b14861c1569856ec628
DYNOHACKER <114745029+DYNOHACKER@users.noreply.github.com>
2022-11-17 23:10:51 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("duski").menu()
+       __import__("dyno").menu()
    except Exception as e:
        exit(str(e))
